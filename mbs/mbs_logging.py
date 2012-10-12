df74527e80b58f9b19830e661ac1c9f4526ea83b
abdul <abdul@mongolab.com>
2012-10-12 14:48:50 -0700

    Temporarily set log level to DEBUG, logging to stdout as well

--- mbs/mbs_logging.py
+++ mbs/mbs_logging.py
@@ -15,9 +15,13 @@ MBS_LOG_DIR = resolve_path("~/.mbs/logs")
 ensure_dir(MBS_LOG_DIR)
 
 logger = logging.getLogger("MBSLogger")
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter("%(levelname)8s | %(asctime)s | %(message)s")
+
+sh = logging.StreamHandler(sys.stdout)
+sh.setFormatter(formatter)
+logger.addHandler(sh)
 
 logfile = os.path.join(MBS_LOG_DIR, "mbs.log")
 fh = TimedRotatingFileHandler(logfile, backupCount=50, when="midnight")
