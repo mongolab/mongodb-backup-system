6002d743fa6d989119cc6c93b518f64b6ffe0dbc
abdul <abdul@mongolab.com>
2012-12-07 12:03:32 -0800

    Revert: Included warned audits in audit report

--- mbs/audit.py
+++ mbs/audit.py
@@ -286,8 +286,6 @@ class PlanAuditor(BackupAuditor):
 
         if failed_plan_reports:
             all_plans_report.failed_audits = failed_plan_reports
-        if warned_plan_reports:
-            all_plans_report.warned_audits = warned_plan_reports
 
         all_plans_report.total_audits = total_plans
         all_plans_report.total_failures = total_failures
@@ -327,9 +325,6 @@ class PlanAuditor(BackupAuditor):
 
         if failed_audits:
             plan_report.failed_audits = failed_audits
-
-        if warned_audits:
-            plan_report.warned_audits = warned_audits
 
         plan_report.total_failures = total_failures
         plan_report.total_audits = total_audits
