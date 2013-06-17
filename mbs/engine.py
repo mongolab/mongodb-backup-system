84f2f07a540cb085165f50191815ea590e90dc13
abdul <abdul@mongolab.com>
2013-06-17 12:02:17 -0700

    Cancel failed tasks every 5 ticks instead of 100 to save time/space

--- mbs/engine.py
+++ mbs/engine.py
@@ -413,9 +413,9 @@ class TaskQueueProcessor(Thread):
         if self._has_available_workers():
             self._start_next_task()
 
-        # Cancel a failed task every 100 ticks and there are available
+        # Cancel a failed task every 5 ticks and there are available
         # workers
-        if self._tick_count % 100 == 0 and self._has_available_workers():
+        if self._tick_count % 5 == 0 and self._has_available_workers():
             self._clean_next_past_due_failed_task()
 
     ###########################################################################
