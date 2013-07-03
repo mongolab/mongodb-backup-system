d7bd8bc5900b1e87f27361622d667d8e7234b5be
abdul <abdul@mongolab.com>
2013-07-02 17:06:10 -0700

    Proper logs dir path resolving

--- mbs/mbs_logging.py
+++ mbs/mbs_logging.py
@@ -7,7 +7,7 @@ import sys
 
 import mbs_config
 
-from utils import ensure_dir
+from utils import ensure_dir, resolve_path
 from logging.handlers import TimedRotatingFileHandler
 import logging
 
@@ -18,7 +18,7 @@ logger = logging.getLogger("MBSLogger")
 
 ###############################################################################
 def setup_logging(log_to_stdout=False, log_file_name="mbs.log"):
-    log_dir = os.path.join(mbs_config.MBS_CONF_DIR, MBS_LOG_DIR)
+    log_dir = resolve_path(os.path.join(mbs_config.MBS_CONF_DIR, MBS_LOG_DIR))
     ensure_dir(log_dir)
 
     logger.setLevel(logging.INFO)
