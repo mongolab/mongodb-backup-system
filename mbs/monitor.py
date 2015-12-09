ce7f4ac885986fbc981f364702adc77abb2b47ca
Greg Banks <gbanks@gmail.com>
2015-12-09 08:45:13 -0800

    formatting

--- mbs/monitor.py
+++ mbs/monitor.py
@@ -16,34 +16,34 @@ import logging
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
-########################################################################################################################
+###############################################################################
 # BackupMonitor
-########################################################################################################################
+###############################################################################
 class BackupMonitor(ScheduleRunner):
     """
         Backup monitoring thread
     """
-    ####################################################################################################################
+    ###########################################################################
     def __init__(self, backup_system):
         self._backup_system = backup_system
         ScheduleRunner.__init__(self, schedule=Schedule(frequency_in_seconds=5*60))
         self._backup_monitor_event_listener = BackupMonitorEventListener(backup_monitor=self)
 
-    ####################################################################################################################
+    ###########################################################################
     def run(self):
         # register event listener with event queue
         get_mbs().event_queue.register_event_listener(self._backup_monitor_event_listener)
         super(BackupMonitor, self).run()
 
-    ####################################################################################################################
+    ###########################################################################
     def tick(self):
         self._notify_on_past_due_scheduled_backups()
 
-    ####################################################################################################################
+    ###########################################################################
     def on_backup_event(self, event):
         logger.info("BACKUP %s finished with state %s" % (event.backup.id, event.state))
 
-    ####################################################################################################################
+    ###########################################################################
     def _notify_on_past_due_scheduled_backups(self):
         """
             Send notifications for jobs that has been scheduled for a period
@@ -68,23 +68,23 @@ class BackupMonitor(ScheduleRunner):
                 get_mbs().send_notification(sbj, msg)
                 break
 
-########################################################################################################################
+###############################################################################
 # BackupMonitorEventListener
-########################################################################################################################
+###############################################################################
 class BackupMonitorEventListener(EventListener):
 
-    ####################################################################################################################
+    ###########################################################################
     def __init__(self, backup_monitor=None):
         super(BackupMonitorEventListener, self).__init__()
         self.event_types = [BackupEventTypes.BACKUP_FINISHED]
         self.name = "BackupMonitorEventListener"
         self._backup_monitor = backup_monitor
 
-    ####################################################################################################################
+    ###########################################################################
     def handle_event(self, event):
         self._backup_monitor.on_backup_event(event)
 
-    ####################################################################################################################
+    ###########################################################################
     def to_document(self, display_only=False):
         doc = super(BackupMonitorEventListener, self).to_document(display_only=display_only)
         doc.update({
