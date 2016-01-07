2f98e9e9c7710fe8af7917dabfcdecfed2bb46fa
abdul <abdul@objectlabs.com>
2016-01-07 14:37:00 -0800

    Export SendgridNotificationHandler

--- mbs/notification/__init__.py
+++ mbs/notification/__init__.py
@@ -1,3 +1,3 @@
-from .handler import EmailNotificationHandler, DEFAULT_NOTIFICATION_SUBJECT
+from .handler import EmailNotificationHandler, DEFAULT_NOTIFICATION_SUBJECT, SendgridNotificationHandler
 from .template import NotificationTemplate
 from .message import get_messages
