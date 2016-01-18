55f8e5baefe8c238fa8ab57dcfb648f4d564d0ce
abdul <abdul@objectlabs.com>
2016-01-17 18:01:06 -0800

    revert

--- mbs/errors.py
+++ mbs/errors.py
@@ -758,7 +758,6 @@ class MBSApiError(Exception):
 ########################################################################################################################
 def raise_dump_error(returncode, error_log_line, last_namespace=None):
     # encode error log line
-    error_log_line = utils.str_to_unicode(error_log_line)
     if (returncode == 245 or
             ("Failed: error creating bson file" in error_log_line and
                 "no such file or directory" in error_log_line)):
