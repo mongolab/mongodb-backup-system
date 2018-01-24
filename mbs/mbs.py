5c1efddd9b7a1af40f6d785909b2b2d55d2ea675
abdul <abdul@objectlabs.com>
2018-01-24 12:27:29 -0800

    cleanup

--- mbs/mbs.py
+++ mbs/mbs.py
@@ -270,7 +270,6 @@ class MBS(object):
     ###########################################################################
     @property
     def api_client(self):
-        logger.info("++++> mbs.api_client (BEGIN)")
         if not self._api_client:
             api_client_conf = self._get_config_value("apiClient")
             if api_client_conf:
@@ -279,7 +278,6 @@ class MBS(object):
                 import mbs_client.client
                 self._api_client = mbs_client.client.backup_system_client()
 
-        logger.info("++++> mbs.api_client (END)")
         return self._api_client
 
     ###########################################################################
